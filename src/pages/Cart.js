59dcddbf704bcb00e110312b64fa5ec798977ede
Jeremy <46427980+JeremyFields@users.noreply.github.com>
2023-01-21 19:16:38 -0700

    Commit 2 - Send order button working

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,27 +1,29 @@
-// import { DataStore } from '@aws-amplify/datastore';
-// import { Order } from '../models';
-// import { Button } from '@aws-amplify/ui-react';
-
-// const Sendorder = async () => {
-//     try {
-//         await DataStore.save(
-//             new Order({
-//                 "customerName": "Jeremy Matthews",
-//                 "customerAddress": "111 S. 999th Ave. Mesa, AZ 85212",
-//                 "games": ["Killer Instinct"]
-//             })
-//         )
-//         alert("Success")
-//     } catch (error) {
-//         alert("Unsuccessful")
-//     }
-// };
+import { DataStore } from '@aws-amplify/datastore';
+import { Order } from '../models';
 
 const Cart = () => {
-    return (<h1>Cart</h1>)
-    // return (<Button onClick={Sendorder()}>Submit Order</Button>
-    // )
-}
+    return (
+        <input
+            type="button"
+            value="Submit Order"
+            onClick={async () => {
+                try {
+                    await DataStore.save(
+                        new Order({
+                            "customerName": "Jeremy Henderson",
+                            "customerAddress": "222 N. 222th Ave. Mesa, AZ 85212",
+                            "games": ["Killer Instinct"]
+                        })
+                    )
+                    alert("Success")
+                } catch (error) {
+                    alert("Unsuccessful")
+                }
+
+            }}
+        />
+    )
+};
 
 
 export default Cart;
