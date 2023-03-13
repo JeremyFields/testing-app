0d3a28c925a566334ed8ae8cc5f51bc832cffbce
Jeremy <46427980+JeremyFields@users.noreply.github.com>
2023-03-12 18:08:34 -0700

    3/12/2023 - Working admin

--- src/pages/Admin.js
+++ src/pages/Admin.js
@@ -1,23 +1,23 @@
-import { CreateGame } from '../ui-components';
+import { GameCreateForm } from '../ui-components';
 import '../App.css';
 import { withAuthenticator, Button } from '@aws-amplify/ui-react';
 import awsExports from '../aws-exports';
-import { Amplify, Auth } from 'aws-amplify';
+import { Amplify } from 'aws-amplify';
 Amplify.configure(awsExports);
 
 const Admin = ({ signOut, user }) => {
-
     return (
         <>
+            <h1>Admin</h1>
             <div>
                 <h4 style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>Add new game to database:</h4>
             </div>
             <div style={{ display: "flex", alignItems: "center", justifyContent: "center" }}>
-                <CreateGame style={{ width: "20rem" }} />
+                <GameCreateForm style={{ width: "20rem" }} />
             </div>
             <Button onClick={signOut}>Sign out</Button>
         </>
-    )
-};
+    );
+}
 
 export default withAuthenticator(Admin);
