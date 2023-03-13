2982ad5910189bb67dfc18c289978ec27d08e841
Jeremy <46427980+JeremyFields@users.noreply.github.com>
2023-03-12 18:21:54 -0700

    3/12/2023 - Removing sign up from admin panel.

--- src/pages/Admin.js
+++ src/pages/Admin.js
@@ -1,23 +1,26 @@
 import { GameCreateForm } from '../ui-components';
 import '../App.css';
-import { withAuthenticator, Button } from '@aws-amplify/ui-react';
+import { Authenticator, Button } from '@aws-amplify/ui-react';
 import awsExports from '../aws-exports';
 import { Amplify } from 'aws-amplify';
 Amplify.configure(awsExports);
 
-const Admin = ({ signOut, user }) => {
+const Admin = () => {
     return (
-        <>
-            <h1>Admin</h1>
-            <div>
-                <h4 style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>Add new game to database:</h4>
-            </div>
-            <div style={{ display: "flex", alignItems: "center", justifyContent: "center" }}>
-                <GameCreateForm style={{ width: "20rem" }} />
-            </div>
-            <Button onClick={signOut}>Sign out</Button>
-        </>
+        <Authenticator hideSignUp={true}>
+            {({ signOut, user }) => (
+                <main>
+                    <h1>Hello, Admin.</h1>
+                    <div>
+                        <h4 style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>Add new game to database:</h4>
+                    </div>
+                    <div style={{ display: "flex", alignItems: "center", justifyContent: "center" }}>
+                        <GameCreateForm style={{ width: "20rem" }} />
+                    </div>
+                    <Button onClick={signOut}>Sign out</Button>
+                </main>
+            )}
+        </Authenticator>
     );
 }
-
-export default withAuthenticator(Admin);
+export default Admin;
